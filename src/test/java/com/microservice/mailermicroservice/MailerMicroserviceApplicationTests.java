6cdb27526d24ea754237d499373610202ede2c6a
Athirson Silva <Athirsonarceus@gmail.com>
2023-02-11 16:06:10 -0300

    Initial commit

---
+++ src/test/java/com/microservice/mailermicroservice/MailerMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.mailermicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailerMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
